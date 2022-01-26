fecbd6c01a3d0c492544fc66078900bef8a758dc
Alejandro <lopalejandro@gmail.com>
2022-01-26 16:06:05 -0300

    tercera version

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from peewee import *
 import datetime
+import sys
+
+from peewee import *
+from collections import OrderedDict
 
 db = SqliteDatabase('diario.db')
-
 
 class Entrada(Model):
     # contenido
@@ -23,19 +25,41 @@ def create_and_connect():
 
 def menu_loop():
     """Show Menu"""
+    choice = None
+    while choice != "q":
+        print("Pulse q para salir")
+        for key, value in menu.items():
+            print("{}) {}".format(key, value.__doc__))
+        choice= input("Action: ").lower().strip()
+
+        if choice in menu:
+            menu[choice]()
 
 
 def add_entrada():
     """Add Entrada"""
+    print("Ingrese su contenido. CTRL + D para terminar")
+    data = sys.stdin.read().strip()
 
+    if data:
+       # if input('Quiere guardar su entrada? [SN]').lower().strip() != 'n':
+       Entrada.create(contenido=data)
+       print("Su entrada ha sido guardada")
 
 def ver_entradas():
     """Ver todas las entradas"""
+    print("Ver")
 
 
-def borrar_entrada(entrada):
+def borrar_entrada():
     """Borrar Entrada"""
+    print("borrar")
 
+menu = OrderedDict([
+    ("a",add_entrada),
+    ("v",ver_entradas),
+    ("b",borrar_entrada)
+])
 
 if __name__ == "__main__":
     create_and_connect()
