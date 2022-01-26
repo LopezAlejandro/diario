ab3dca127a0454ec3c5fb19753fcd21833584095
Alejandro <lopalejandro@gmail.com>
2022-01-26 14:45:56 -0300

    segunda subida

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 from peewee import *
+import datetime
 
-db=SqliteDatabase("diario.db")
+db = SqliteDatabase('diario.db')
 
 
 class Entrada(Model):
-    #contenido
-    #timesramp
+    # contenido
+    # timesramp
+    contenido = TextField()
+    timestamp = DateTimeField(default=datetime.datetime.now())
 
     class Meta:
-        databaase=db
+        database = db
+
+
+def create_and_connect():
+    """Se conecta y crea la base de datos"""
+
+    db.connect()
+    db.create_tables([Entrada])
+
 
 def menu_loop():
     """Show Menu"""
 
+
 def add_entrada():
     """Add Entrada"""
+
 
 def ver_entradas():
     """Ver todas las entradas"""
 
+
 def borrar_entrada(entrada):
     """Borrar Entrada"""
 
+
+if __name__ == "__main__":
+    create_and_connect()
+    menu_loop()
